33be8d1592b7f696b8511ed21c96a0d0c4fe19e0
Sohil Sathe <sathe.sohil@gmail.com>
2024-11-08 18:19:44 -0800

    Clean up basic UI

--- Film Room/ContentView.swift
+++ Film Room/ContentView.swift
@@ -40,14 +40,15 @@ struct ContentView: View {
     @Query private var items: [Item]
 
     @State private var youtubeLink: String = ""
-    @State private var isVideoDownloaded: Bool = false
+    @State private var downloadInProgress: Bool = false
+    @State private var downloadComplete: Bool = false
     @State private var videoURL: URL? = nil
     
     let destinationPath = getVideosDirectory()?.path() ?? "~/Downloads/"
     
     var body: some View {
         VStack {
-            if isVideoDownloaded, let videoURL = videoURL {
+            if downloadComplete, let videoURL = videoURL {
                 // Show video player if the video is downloaded
                 PlayerView(videoURL: videoURL)
                     .background(Color.black)
@@ -57,34 +58,52 @@ struct ContentView: View {
                 // Default view with title, text field, and button
                 VStack {
                     Text("Film Room")
-                        .font(.largeTitle)
+                        .font(.system(size: 64))  // Make the font size larger
                         .foregroundColor(.white)
                         .padding(.top, 50)
                     
                     HStack {
                         TextField("YouTube link", text: $youtubeLink)
                             .padding()
+                            .font(.system(size: 32))  // Make the font size larger
                             .cornerRadius(8)
-                            .foregroundColor(.black)
+                            .foregroundColor(.white)
+                            .background(.black)
                             .padding(.horizontal, 20)
                         
                         Button(action: {
-                            downloadYoutubeVideo(url: youtubeLink, destinationPath: destinationPath, videoName: VIDEO_NAME) { success, msg in
-                                if success {
-                                    self.videoURL = URL(fileURLWithPath: destinationPath).appendingPathComponent("\(VIDEO_NAME).\(VIDEO_EXT)")
-                                    self.isVideoDownloaded = true
-                                    print ("download successful to \(destinationPath) with message \(msg ?? "no msg")")
-                                } else {
-                                    print("download unsuccessful with msg \(msg ?? "no msg")")
+                            //TODO: refactor out
+                            downloadInProgress = true
+                            DispatchQueue.global().async {
+                                downloadYoutubeVideo(url: youtubeLink, destinationPath: destinationPath, videoName: VIDEO_NAME) { success, msg in
+                                    if success {
+                                        DispatchQueue.main.async {
+                                            videoURL = URL(fileURLWithPath: destinationPath).appendingPathComponent("\(VIDEO_NAME).\(VIDEO_EXT)")
+                                            downloadInProgress = false
+                                            downloadComplete = true
+                                            print ("download successful to \(destinationPath) with message \(msg ?? "no msg")")
+                                        }
+                                    } else {
+                                        DispatchQueue.main.async {
+                                            print("download unsuccessful with msg \(msg ?? "no msg")")
+                                            downloadInProgress = false
+                                        }
+                                    }
                                 }
-                                
                             }
                         }) {
-                            Circle()
-                                .fill(Color.blue)
-                                .frame(width: 50, height: 50)
-                                
+                            if downloadInProgress {
+                                ProgressView()
+                                    .progressViewStyle(CircularProgressViewStyle(tint: .green))
+                                    .frame(width: 100, height: 100)
+                            } else {
+                                Circle()
+                                    .fill(.green)
+                                    .frame(width: 100, height: 100)
+                            }
+
                         }
+                        .buttonStyle(PlainButtonStyle())
                         .padding(.trailing, 20)
                         
                     }
@@ -108,7 +127,7 @@ struct PlayerView: View {
     }
 }
 
-#Preview {
-    ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
-}
+//#Preview {
+//    ContentView()
+//        .modelContainer(for: Item.self, inMemory: true)
+//}
